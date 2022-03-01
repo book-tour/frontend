7270457e75ffdb9468fe3d28c51e30e4b990932e
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-03-01 11:01:18 +0700

    fix login ui

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,6 +2,8 @@ import { GoogleLogin } from 'react-google-login';
 import './login.css'
 import { alpha, styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography'
+
 
 function Login() {
 
@@ -12,6 +14,9 @@ function Login() {
     const CssTextField = styled(TextField)({
         '&': {
             margin: '15px 0 5px 0',
+        },
+        '& input': {
+            color: 'white',
         },
         '& label.Mui-focused': {
             color: 'white',
@@ -44,8 +49,8 @@ function Login() {
                 <div className='container'>
                     <p className='title-login'>Tugo</p>
                     <div className='input-info'>
-                        <CssTextField label="Username" id="custom-css-outlined-input" />
-                        <CssTextField label="Password" id="custom-css-outlined-input" />
+                        <CssTextField label="Username" id="custom-css-outlined-input" required/>
+                        <CssTextField label="Password" id="custom-css-outlined-input" type="password" required />
                         <a href=''>Forgot password?</a>
                         <button>Login</button>
                         <p>
@@ -55,11 +60,17 @@ function Login() {
                     </div>
                     <hr />
                     <p>Or</p>
-                    <GoogleLogin className='google-login'
+                    <GoogleLogin
                         clientId={global.config.KEY}
                         onSuccess={googleResponse}
                         cookiePolicy={'single_host_origin'}
                         buttonText="Login with google"
+                        render={renderProps => (
+                            <button onClick={renderProps.onClick} disabled={renderProps.disabled} className='google-login'>
+                                <img src={require('../../assets/google_logo.png')} alt="google" />
+                                <span>Login with google</span>
+                            </button>
+                        )}
                     ></GoogleLogin>
                 </div>
             </div>
